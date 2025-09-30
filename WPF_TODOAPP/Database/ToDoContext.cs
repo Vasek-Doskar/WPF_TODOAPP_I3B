b678ac9f0e34e6bdac0a6f006754f70279a0fc37
Vasek-Doskar <doskar@spsjc.cz>
2025-09-30 15:12:06 +0200

    Přidejte soubory projektu.

---
+++ WPF_TODOAPP/Database/ToDoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WPF_TODOAPP.Models;
+
+namespace WPF_TODOAPP.Database
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ToDoEntity> ToDoSet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data Source = ToDoAppDb.db");
+        }
+    }
+}
---
+++ WPF_TODOAPP/Models/ToDoEntity.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace WPF_TODOAPP.Models
+{
+    public class ToDoEntity : INotifyPropertyChanged
+    {
+        private bool _isDone;
+
+        public ToDoEntity(string title, bool isDone = false)
+        {
+            IsDone = isDone;
+            Title = title;
+        }
+
+        public bool IsDone
+        {
+            get => _isDone;
+            set
+            {
+                _isDone = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsDone)));
+            }
+        }
+        public string Title { get; private set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public override string ToString() => Title;
+    }
+}
